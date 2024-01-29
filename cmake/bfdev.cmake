a39c864ce1643c03a7bc5dffd43371e9fe852b1c
ffashion <helloworldffashion@gmail.com>
2024-01-29 16:03:14 +0800

    feat adbd: remove bfdev to lib dir

--- cmake/bfdev.cmake
+++ cmake/bfdev.cmake
@@ -1,7 +1,6 @@
 set(BFDEV_SOURCE_PATH ${PROJECT_SOURCE_DIR}/src)
 
 # FIXME: support use system lib and user specific lib
-add_subdirectory(src/bfdev)
-include_directories(${PROJECT_SOURCE_DIR}/src/bfdev/include)
-include_directories(${PROJECT_BINARY_DIR}/src/bfdev/generated)
-include_directories(${PROJECT_SOURCE_DIR}/src)
+add_subdirectory(lib/bfdev)
+include_directories(${PROJECT_SOURCE_DIR}/lib/bfdev/include)
+include_directories(${PROJECT_BINARY_DIR}/lib/bfdev/generated)
