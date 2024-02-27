994295d4b8ca50a650741f68e0f10ab8e8746898
John Sanpe <sanpeqf@gmail.com>
2024-02-28 00:51:20 +0800

    refactor mempool: added bfdev allocpool optimize
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- src/core/xdbd_pool.h
+++ src/core/xdbd_pool.h
@@ -1,19 +1,55 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2024 John Sanpe <sanpeqf@gmail.com>
+ */
+
 #ifndef __XDBD_POOL__H__
 #define __XDBD_POOL__H__
 
-#include <bfdev/list.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <bfdev/allocator.h>
+#include <bfdev/allocpool.h>
+#include <bfdev/slist.h>
 
-typedef struct {
-    void *buf;
-    bfdev_list_head_t list;
-} xdbd_pool_node_t;
+#ifndef XDBD_PAGE_SIZE
+# define XDBD_PAGE_SIZE 4096
+#endif
 
-typedef struct {
-    bfdev_list_head_t head;
-} xdbd_pool_t;
+#ifndef XDBD_ALIGN_SIZE
+# define XDBD_ALIGN_SIZE BFDEV_BYTES_PER_LONG
+#endif
 
-xdbd_pool_t *xdbd_create_pool();
-void *xdbd_palloc(xdbd_pool_t *pool, unsigned size);
-void xdbd_destroy_pool(xdbd_pool_t *pool);
+typedef struct xdbd_pool xdbd_pool_t;
+typedef struct xdbd_cache xdbd_cache_t;
+typedef struct xdbd_block xdbd_block_t;
+
+struct xdbd_pool {
+    bfdev_slist_head_t cache;
+    bfdev_slist_head_t block;
+};
+
+struct xdbd_cache {
+    bfdev_slist_head_t list;
+    bfdev_allocpool_t pool;
+    void *data;
+};
+
+struct xdbd_block {
+    bfdev_slist_head_t list;
+    void *data;
+};
+
+extern __bfdev_malloc void *
+xdbd_palloc(xdbd_pool_t *pool, size_t size);
+
+extern xdbd_pool_t *
+xdbd_create_pool();
+
+extern void
+xdbd_release_pool(xdbd_pool_t *pool);
+
+extern void
+xdbd_destroy_pool(xdbd_pool_t *pool);
 
 #endif  /*__XDBD_POOL__H__*/
