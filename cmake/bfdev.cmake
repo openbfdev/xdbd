bcf849a4772fb10ce8741b3b257fa0e877979a7e
John Sanpe <sanpeqf@gmail.com>
2024-03-18 12:21:39 +0800

    feat bfdev: use built-in header reference path
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- cmake/bfdev.cmake
+++ cmake/bfdev.cmake
@@ -1,7 +1,10 @@
-set(BFDEV_SOURCE_PATH ${PROJECT_SOURCE_DIR}/src)
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright(c) 2024 John Sanpe <sanpeqf@gmail.com>
+#
 
-# FIXME: support use system lib and user specific lib
+set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
 add_subdirectory(lib/bfdev)
-include_directories(${PROJECT_SOURCE_DIR}/lib/bfdev/include)
-include_directories(${PROJECT_BINARY_DIR}/lib/bfdev/generated)
-include_directories(${CMAKE_SOURCE_DIR}/lib/bfdev/arch/generic/include)
+
+get_property(BFDEV_INCLUDE_DIRS GLOBAL PROPERTY "BFDEV_INCLUDE_DIRS")
+include_directories(${BFDEV_INCLUDE_DIRS})
